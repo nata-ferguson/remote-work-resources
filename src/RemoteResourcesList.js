363079593e5fca1fa2c60d878c27402c83633bf6
Natasha Ferguson <nata.ferguson@outlook.com>
2023-09-10 19:01:58 -0500

    implemented pagination

--- src/RemoteResourcesList.js
+++ src/RemoteResourcesList.js
@@ -6,6 +6,10 @@ function RemoteResourcesList({ searchQuery }) {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  // pagination state
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(9);
+
   useEffect(() => {
     // This will only run once
     fetch("https://remote-work-resources-api.vercel.app/api/remoteWorkResource")
@@ -36,12 +40,42 @@ function RemoteResourcesList({ searchQuery }) {
     return <div>Loading...</div>;
   }
 
+  // Implementing pagination
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPageCount = Math.ceil(filteredData.length / itemsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
       <div className="resource-list">
-        {filteredData.map((resource) => (
+        {currentItems.map((resource) => (
           <ResourceItem key={resource._id} resource={resource} />
         ))}
+      </div>
+
+      <div className="pagination">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span>
+          Page {currentPage} of {totalPageCount}
+        </span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={
+            currentPage === Math.ceil(filteredData.length / itemsPerPage)
+          }
+        >
+          Next
+        </button>
       </div>
     </div>
   );
