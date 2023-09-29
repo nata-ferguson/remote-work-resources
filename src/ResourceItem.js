2514ea70d8de8ab472160251ff486c727e443fcc
Natasha Ferguson <nata.ferguson@outlook.com>
2023-09-28 22:02:52 -0500

    added social sharing

--- src/ResourceItem.js
+++ src/ResourceItem.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 export default function ResourceItem({ resource, viewType }) {
+  const currentURL = window.location.href;
+  //encoding the url and resource name for sharing
+  const encodedURL = encodeURIComponent(currentURL);
+  const encodedName = encodeURIComponent(resource.name);
+
   return (
     <div className={`resource-card ${viewType === "list" ? "list-item" : ""}`}>
       <h1>
@@ -15,6 +20,37 @@ export default function ResourceItem({ resource, viewType }) {
       </div>
 
       <p>{resource.description}</p>
+
+      <div className="social-sharing">
+        <a
+          href={`https://www.facebook.com/sharer/sharer.php?u=${encodedURL}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Share on Facebook
+        </a>
+        <a
+          href={`https://twitter.com/intent/tweet?text=${encodedName}&url=${encodedURL}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Share on Twitter
+        </a>
+        <a
+          href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodedURL}&title=${encodedName}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Share on LinkedIn
+        </a>
+        <a
+          href={`whatsapp://send?text=${encodedName} ${encodedURL}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Share on WhatsApp
+        </a>
+      </div>
     </div>
   );
 }
